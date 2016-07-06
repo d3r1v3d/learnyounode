ff98ecec4fc60e537ba4a1922314fc01b9ae2a15
d3r1v3d <d3r1v3d@gmail.com>
2016-07-06 14:25:49 -0600

    Solution for challenge 13 - HTTP JSON API server.

---
+++ dist/13.http_json_api_server.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var http = require('http');
+var url = require('url');
+
+var listen_port = process.argv[2];
+
+function usage() {
+  console.log('Usage: ' + process.argv[0] + ' ' + process.argv[1] + ' <port>');
+}
+if (!listen_port) {
+  usage();
+  process.exit(1);
+}
+
+var api_server = http.createServer(function (request, response) {
+  var request_url = url.parse(request.url, true);
+  var api_response = '';
+
+  if (request_url.query.iso) {
+    var iso_date = new Date(request_url.query.iso);
+
+    switch (request_url.pathname) {
+      case '/api/parsetime':
+        api_response = {
+          'hour': iso_date.getHours(),
+          'minute': iso_date.getMinutes(),
+          'second': iso_date.getSeconds()
+        };
+        break;
+      case '/api/unixtime':
+        api_response = {
+          'unixtime': iso_date.getTime()
+        };
+        break;
+      default:
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+
+    response.writeHead(200, {
+      'content-type': 'application/json'
+    });
+    response.end(JSON.stringify(api_response));
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
+});
+api_server.listen(Number(listen_port));
